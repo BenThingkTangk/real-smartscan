d1cb875a3f0bb903281d9d025b8fab0795fe99e1
BenThingkTangk <ben.oleary@thingktangk.com>
2026-03-29 02:27:10 +0000

    Fix: CommonJS exports for Vercel Node functions + pick up OPENAI_API_KEY + SERP_API_KEY env vars

--- api/explain.js
+++ api/explain.js
@@ -5,7 +5,7 @@
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY || '';
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
--- api/search.js
+++ api/search.js
@@ -4,7 +4,7 @@
 
 const SERP_API_KEY = process.env.SERP_API_KEY || '';
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
